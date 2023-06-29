b94b2c3172b3ea12e14a8b134a9608eff1737d45
oOBobbyOo <944831575@qq.com>
2023-06-29 12:30:16 +0800

    chore: add tailwind

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,5 +3,13 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-recess-order',
     'stylelint-config-prettier'
-  ]
+  ],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'apply']
+      }
+    ]
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './index.html'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
